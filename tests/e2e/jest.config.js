3556c7e1fdff17ce4d7a930345d0d712d7709f16
Shanjidah Afroz <shanjidahafroz885@gmail.com>
2021-12-15 11:25:05 +0600

    Jest Configuration

---
+++ tests/e2e/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    preset: "jest-puppeteer",
+    //globals: { URL: "<http://localhost:3000>" },
+        verbose: true,
+    transform: {},
+    
+   // globalSetup: './setup.js',
+    
+    
+  };
+  
+  
+  
+  
